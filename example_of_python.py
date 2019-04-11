3faf5343a6a46f91b2a14903284bd3a232d93069
lexir <ivanboldyrev2003@gmail.com>
2019-04-11 20:51:47 +0300

    Use constant and

--- example_of_python.py
+++ example_of_python.py
@@ -7,3 +7,12 @@ age = 57
 name = "Peso"
 print('Возраст {0} -- {1} лет'.format(name, age))
 print("Почему{0} забавляется с этим Python?".format(name))
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''Это многосьрочная строка.
+Это вторая её строчка.'''
+print(s)
+
