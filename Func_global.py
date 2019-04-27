25c15ba7f1b8ce0e62463f9fbac6f7abf509a89d
lexir <ivanboldyrev2003@gmail.com>
2019-04-27 20:53:03 +0300

    global

---
+++ Func_global.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+x = 50
+
+def func():
+    global x
+
+    print('x равно', x)
+    x = 2
+    print('Заменяем глобальное значение x на', x)
+
+func()
+print('Значение x составляет', x)
